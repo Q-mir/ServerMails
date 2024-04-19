f2a34003876ee6b7d48ed34e99cddf6b9538035d
Вячеслав Сквороцов <swank123@yandex.ru>
2024-04-19 19:24:40 +0700

    Добавьте файлы проекта.

---
+++ Data/Connection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Data;
+
+    public class Connection : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        string path = $@"Data Source=(localdb)\MSSQLLocalDB;
+                         Initial Catalog=db.ServerMails;
+                         Integrated Security=True;
+                         Trust Server Certificate=False;";
+            optionsBuilder.UseSqlServer(path);
+        }
+    }
